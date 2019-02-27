b0719644557ff028dba45db44cdc8da15c07e63f
zhaogaojiann <zgj@DXPER>
2019-02-27 12:01:23 +0800

    封装对象

--- js/components/accordion.js
+++ js/components/accordion.js
@@ -1,113 +1,118 @@
-$("dt.jgui-accordion-navitem").click(function(event) {
-  $(this).removeClass("selected");
-  $(this)
-    .siblings("dd")
-    .slideToggle(300, function() {
-      if ($(this).is(":hidden")) {
-        $(this)
-          .siblings("dt")
-          .children()
-          .children(".jgui-accordion-navitem-more")
-          .removeClass("expanded", 0);
-      } else {
-        $(this)
-          .siblings("dt")
-          .children()
-          .children(".jgui-accordion-navitem-more")
-          .addClass("expanded", 0);
-      }
-    });
-  $(this)
-    .closest(".jgui-accordion")
-    .find(".jgui-accordion-navitem")
-    .removeClass("selected");
-  $(this)
-    .closest(".jgui-accordion")
-    .find(".jgui-accordion-navitem-child")
-    .removeClass("selected");
-  $(this).addClass("selected");
-  stopPropagation(event);
-});
-
-$(".jgui-accordion-navitem-child").click(function(event) {
-  $(this)
-    .closest(".jgui-accordion")
-    .find(".jgui-accordion-navitem")
-    .removeClass("selected");
-  $(this)
-    .closest(".jgui-accordion")
-    .find(".jgui-accordion-navitem-child")
-    .removeClass("selected");
-  $(this).addClass("selected");
-  stopPropagation(event);
-});
-//电脑端中键滚动事件
-var mousewheel =
-  getBrowserInfo() == "Firefox" ? "DOMMouseScroll" : "mousewheel";
-$(".jgui-accordion").on(mousewheel, function(event) {
-  var delta = 0;
-  var direction = 0;
-  if (!event)
-    /* For IE. */
-    event = window.event;
-  if (event.originalEvent.wheelDelta) {
-    /* IE/Opera. */
-    delta = event.originalEvent.wheelDelta / 120;
-  } else if (event.originalEvent.detail) {
-    delta = -event.originalEvent.detail / 3;
-  }
-  if (delta) {
-    var datas = $(this).data("datas");
-    datas._sumdelta += delta > 0 ? -1 : 1;
-    datas._startmousewheeldatetime = new Date().valueOf();
-    var obj = $(this);
-    var handle = function() {
-      var step = Math.floor(obj.height() / 10); //可视区高度
-      var cur_top = obj.scrollTop(); //当前滚过的高度
-      if (
-        new Date().valueOf() - datas._startmousewheeldatetime > 100 &&
-        datas._sumdelta != 0
-      ) {
-        //100ms内没有移动滚轮
-        direction = datas._sumdelta;
-        datas._sumdelta = 0;
-        obj.stop().animate(
-          {
-            scrollTop: direction * Math.abs(direction) * step + cur_top
-          },
-          400,
-          "linear",
-          function() {
-            clearInterval(datas._mouseintervalhandle);
-            datas._mouseintervalhandle = undefined;
-          }
-        );
-      }
-    };
-    if (datas._mouseintervalhandle == undefined) {
-      datas._mouseintervalhandle = setInterval(handle, 10);
+// 初始化内容
+$(function() {
+  //目录点击事件
+  $(".jgui-accordion dt.jgui-accordion-navitem").click(function(event) {
+    $(this).removeClass("selected");
+    $(this)
+      .siblings("dd")
+      .slideToggle(300, function() {
+        if ($(this).is(":hidden")) {
+          $(this)
+            .siblings("dt")
+            .children()
+            .children(".jgui-accordion-navitem-more")
+            .removeClass("expanded", 0);
+        } else {
+          $(this)
+            .siblings("dt")
+            .children()
+            .children(".jgui-accordion-navitem-more")
+            .addClass("expanded", 0);
+        }
+      });
+    $(this)
+      .closest(".jgui-accordion")
+      .find(".jgui-accordion-navitem")
+      .removeClass("selected");
+    $(this)
+      .closest(".jgui-accordion")
+      .find(".jgui-accordion-navitem-child")
+      .removeClass("selected");
+    $(this).addClass("selected");
+    stopPropagation(event);
+  });
+  //菜单内容点击事件
+  $(".jgui-accordion-navitem-child").click(function(event) {
+    $(this)
+      .closest(".jgui-accordion")
+      .find(".jgui-accordion-navitem")
+      .removeClass("selected");
+    $(this)
+      .closest(".jgui-accordion")
+      .find(".jgui-accordion-navitem-child")
+      .removeClass("selected");
+    $(this).addClass("selected");
+    stopPropagation(event);
+  });
+  //电脑端中键滚动事件
+  var mousewheel =
+    getBrowserInfo() == "Firefox" ? "DOMMouseScroll" : "mousewheel";
+  $(".jgui-accordion").on(mousewheel, function(event) {
+    var delta = 0;
+    var direction = 0;
+    if (!event)
+      /* For IE. */
+      event = window.event;
+    if (event.originalEvent.wheelDelta) {
+      /* IE/Opera. */
+      delta = event.originalEvent.wheelDelta / 120;
+    } else if (event.originalEvent.detail) {
+      delta = -event.originalEvent.detail / 3;
     }
-  }
-  stopPropagation(event);
-});
-//手机端鼠标拖动事件
-$(".jgui-accordion").on("touchstart", function(e) {
-  startY = e.originalEvent.changedTouches[0].pageY;
-});
-$(".jgui-accordion").on("touchmove", function(e) {
-  e.preventDefault();
-  (moveEndY = e.originalEvent.changedTouches[0].pageY), (Y = moveEndY - startY);
-  startY = moveEndY;
-  var cur_top = $(this).scrollTop(); //当前滚过的高度
-  $(this)
-    .stop()
-    .animate({ scrollTop: -Y + cur_top }, 0);
+    if (delta) {
+      var datas = $(this).data("datas");
+      datas._sumdelta += delta > 0 ? -1 : 1;
+      datas._startmousewheeldatetime = new Date().valueOf();
+      var $obj = $(this);
+      var handle = function() {
+        var step = Math.floor($obj.height() / 10); //可视区高度
+        var cur_top = $obj.scrollTop(); //当前滚过的高度
+        if (
+          new Date().valueOf() - datas._startmousewheeldatetime > 100 &&
+          datas._sumdelta != 0
+        ) {
+          //100ms内没有移动滚轮
+          direction = datas._sumdelta;
+          datas._sumdelta = 0;
+          $obj.stop().animate(
+            {
+              scrollTop: direction * Math.abs(direction) * step + cur_top
+            },
+            400,
+            "linear",
+            function() {
+              clearInterval(datas._mouseintervalhandle);
+              datas._mouseintervalhandle = undefined;
+            }
+          );
+        }
+      };
+      if (datas._mouseintervalhandle == undefined) {
+        datas._mouseintervalhandle = setInterval(handle, 10);
+      }
+    }
+    stopPropagation(event);
+  });
+  //手机端鼠标拖动事件
+  $(".jgui-accordion").on("touchstart", function(e) {
+    startY = e.originalEvent.changedTouches[0].pageY;
+  });
+  $(".jgui-accordion").on("touchmove", function(e) {
+    e.preventDefault();
+    (moveEndY = e.originalEvent.changedTouches[0].pageY),
+      (Y = moveEndY - startY);
+    startY = moveEndY;
+    var cur_top = $(this).scrollTop(); //当前滚过的高度
+    $(this)
+      .stop()
+      .animate({ scrollTop: -Y + cur_top }, 0);
+  });
 });
 //Accordion封装
 J.Accordion = (function($) {
-  init = function(selector, p_options, p_datas, p_param) {
-    return selector.each(function() {
-      var datas = selector.extend(
+  init = function($p_selector, p_options, p_datas, p_param) {
+    return $p_selector.each(function() {
+      var datas = $p_selector.extend(
         {
           _sumdelta: 0,
           _mouseintervalhandle: undefined,
@@ -115,33 +120,32 @@ J.Accordion = (function($) {
         },
         p_datas
       );
-      selector.data("datas", datas);
+      $p_selector.data("datas", datas);
     });
   };
-  //这里只获取第一层navitem就可以，回头代码需要改进
   //折叠
-  fold = function(selector) {
-    return selector.each(function() {
-      selector
+  fold = function($p_selector) {
+    return $p_selector.each(function() {
+      $p_selector
         .find(".jgui-accordion-navitem")
         .siblings("dd")
         .slideUp();
-      selector.find(".jgui-accordion-navitem span").hide();
-      selector
+      $p_selector.find(".jgui-accordion-navitem span").hide();
+      $p_selector
         .find(".jgui-accordion-navitem .jgui-accordion-navitem-more")
         .hide();
     });
   };
   //展开
-  unfold = function(selector) {
-    return selector.each(function() {
-      selector
+  unfold = function($p_selector) {
+    return $p_selector.each(function() {
+      $p_selector
         .find(".jgui-accordion-navitem-more.expanded")
         .closest(".jgui-accordion-navitem")
         .siblings("dd")
         .slideDown();
-      selector.find(".jgui-accordion-navitem span").show();
-      selector
+      $p_selector.find(".jgui-accordion-navitem span").show();
+      $p_selector
         .find(".jgui-accordion-navitem .jgui-accordion-navitem-more")
         .show();
     });
--- js/page/index.js
+++ js/page/index.js
@@ -1,26 +1,29 @@
-$('#folderbtn').click(function(event) {
-    if($('#leftpanel').is('.unfold')){//未折叠
-        $('#leftpanel').width(50);
-        $('#centerpanel').css('left','50px');
-        $('#mainlogo').html('J');
-        J.Accordion.fold($('#menuaccordion'));
+$(function() {
+  // 初始化内容
+  $("#folderbtn").click(function(event) {
+    if ($("#leftpanel").is(".unfold")) {
+      //未折叠
+      $("#leftpanel").width(50);
+      $("#centerpanel").css("left", "50px");
+      $("#mainlogo").html("J");
+      J.Accordion.fold($("#menuaccordion"));
+    } else {
+      $("#leftpanel").width(300);
+      $("#centerpanel").css("left", "300px");
+      $("#mainlogo").html("JGUI DEMO");
+      J.Accordion.unfold($("#menuaccordion"));
     }
-    else{
-        $('#leftpanel').width(300);
-        $('#centerpanel').css('left','300px');
-        $('#mainlogo').html('JGUI DEMO');
-        J.Accordion.unfold($('#menuaccordion'));
-    }
-    $('#leftpanel').toggleClass('unfold');
-    $('#folderbtn').toggleClass('icon-menu-unfold icon-menu-fold');
-});
-$("#menuaccordion .jgui-accordion-navitem").click(function(event) {
-    if(!$('#leftpanel').is('.unfold')){
-        $('#leftpanel').width(300);
-        $('#centerpanel').css('left','300px');
-        $('#mainlogo').html('JGUI DEMO');
-        J.Accordion.unfold($('#menuaccordion'));
-        $('#leftpanel').toggleClass('unfold');
-        $('#folderbtn').toggleClass('icon-menu-unfold icon-menu-fold');
+    $("#leftpanel").toggleClass("unfold");
+    $("#folderbtn").toggleClass("icon-menu-unfold icon-menu-fold");
+  });
+  $("#menuaccordion .jgui-accordion-navitem").click(function(event) {
+    if (!$("#leftpanel").is(".unfold")) {
+      $("#leftpanel").width(300);
+      $("#centerpanel").css("left", "300px");
+      $("#mainlogo").html("JGUI DEMO");
+      J.Accordion.unfold($("#menuaccordion"));
+      $("#leftpanel").toggleClass("unfold");
+      $("#folderbtn").toggleClass("icon-menu-unfold icon-menu-fold");
     }
   });
+});
